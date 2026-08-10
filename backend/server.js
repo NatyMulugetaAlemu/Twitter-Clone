a11bb888b108ddbaa5f816bdb34af8905a39099c
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-08-09 23:30:20 -0900

    2.Implement MongoDB connection and update signup controller structure

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 import authRoutes from "./routes/authRoutes.js"
+import connectMONGODB from "./db/connectMONGODB.js"
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const app=express()
 
 const PORT=process.env.PORT
 
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 
 app.get("/",(req,res)=>{
@@ -14,5 +18,7 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(PORT,()=>{
+    connectMONGODB()
     console.log(`Server is running on PORT:${PORT}`)
 })
+
